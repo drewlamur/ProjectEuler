a035bee2c2a5d48683b658f3e76c54c16d29c80d
drewlamur <wes.wallace.github@gmail.com>
2019-12-28 08:40:32 -0500

    pe - p1 addition, p13 solution in elixir

--- euler_p1.exs
+++ euler_p1.exs
@@ -1,7 +1,7 @@
 # Multiples of 3 and 5
 # Problem # 1
 
-defmodule ProblemOne do
+defmodule ProblemOneA do
   def getMultiples() do
     nums = Enum.to_list 1..999
     Enum.filter(nums, fn num -> 
@@ -9,8 +9,24 @@ defmodule ProblemOne do
     end)
   end
   def getMultiplesSum() do
-    Enum.sum(ProblemOne.getMultiples())
+    Enum.sum(ProblemOneA.getMultiples())
   end
 end
 
-IO.puts ProblemOne.getMultiplesSum
+IO.puts ProblemOneA.getMultiplesSum
+
+# Example, using pipe operator
+# Initialize a nums list, 1 to 999
+# Filter the list to multiples of 3 and 5
+# Pipe the filtered list to sum
+
+defmodule ProblemOneB do
+  def getMultiplesSum() do
+    nums = Enum.to_list 1..999
+    Enum.filter(nums, fn num ->
+        rem(num, 3) === 0 || rem(num, 5) === 0
+    end) |> Enum.sum()
+  end
+end
+
+IO.puts ProblemOneB.getMultiplesSum
